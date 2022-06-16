b0f29ef12598d83363c19683fbf42dbc7f831e88
jeremie klinger <jeremie.klinger@gmail.com>
2022-06-16 14:04:36 +0200

    hotfix

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -1,5 +1,5 @@
 local keymap = vim.api.nvim_set_keymap
-local opts={noremap=true}
+local opts = { noremap = true }
 
 -- set leader key
 -- --Remap space as leader key
@@ -8,31 +8,28 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- window navigation keymaps
-keymap('n','<C-j>',':wincmd j<CR>',opts)
-keymap('n','<C-k>',':wincmd k<CR>',opts)
-keymap('n','<C-l>',':wincmd l<CR>',opts)
-keymap('n','<C-h>',':wincmd h<CR>',opts)
+keymap('n', '<C-j>', ':wincmd j<CR>', opts)
+keymap('n', '<C-k>', ':wincmd k<CR>', opts)
+keymap('n', '<C-l>', ':wincmd l<CR>', opts)
+keymap('n', '<C-h>', ':wincmd h<CR>', opts)
 
 -- window sizing keymaps
-keymap('n','<S-Up>',':resize -2<CR>',opts)
-keymap('n','<S-Down>',':resize +2<CR>',opts)
-keymap('n','<S-Left>',': vertical resize -2<CR>',opts)
-keymap('n','<S-Right>',': vertical resize +2<CR>',opts)
+keymap('n', '<S-Up>', ':resize -2<CR>', opts)
+keymap('n', '<S-Down>', ':resize +2<CR>', opts)
+keymap('n', '<S-Left>', ': vertical resize -2<CR>', opts)
+keymap('n', '<S-Right>', ': vertical resize +2<CR>', opts)
 
 -- buffer navigation keymaps
-keymap('n','<S-l>',':bnext <CR>',opts)
-keymap('n','<S-h>',':bprev <CR>',opts)
-keymap('n','<leader>c',':w<CR>:bdelete<CR>',opts)
+keymap('n', '<S-l>', ':bnext <CR>', opts)
+keymap('n', '<S-h>', ':bprev <CR>', opts)
+keymap('n', '<leader>c', ':w<CR>:bdelete<CR>', opts)
 
 
 -- QOL keympas
-keymap('i','kj','<ESC>',opts) -- going fom insert to normal quicler
-keymap('i','jk','<ESC>',opts)
-keymap('v','$','$h',opts) -- to select until end of line without the cariage
-keymap('n','<leader>m',':NvimTreeToggle<CR>',opts) -- open menu
-keymap('n','<leader>f',':Format<CR>',opts) -- format (not working fot now)
-keymap('n',':hf<CR>',':!ls',opts) -- format (not working fot now)
+keymap('i', 'kj', '<ESC>', opts) -- going fom insert to normal quicler
+keymap('i', 'jk', '<ESC>', opts)
+keymap('v', '$', '$h', opts) -- to select until end of line without the cariage
+keymap('n', '<leader>m', ':NvimTreeToggle<CR>', opts) -- open menu
+keymap('n', '<leader>f', ':Format<CR>', opts) -- format (not working fot now)
+keymap('n', ':hf<CR>', ':!ls', opts) -- format (not working fot now)
 
--- git hotfix cmd
-local cmd = vim.cmd
-cmd([[command! Hotfix !mylazygit "hotfix"]])
